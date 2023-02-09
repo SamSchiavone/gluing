4cb35176be4879af1532182d7c8a920056c78e04
Sam Schiavone <sam.schiavone@gmail.com>
2023-02-08 20:20:09 -0500

    more recovery example

--- examples/genus4/recovery.m
+++ examples/genus4/recovery.m
@@ -9,13 +9,30 @@ R2<u,v> := PolynomialRing(QQ,2);
 p := Resultant(Q,F,w);
 p := Evaluate(p, [R2.1, R2.2, 1, 0]);
 S := RiemannSurface(p);
+Cplane := Curve(Spec(R2), p);
 Genus(S);
 Pi := BigPeriodMatrix(S);
 tau := SmallPeriodMatrix(S);
-omegas := S`DFF;
+//omegas := S`DFF;
+// or omegas := HolomorphicDifferentials(S); ?
 cs := [];
 for i := 1 to 4 do
   dz := [0,0,0,0];
   dz[i] := 1;
   Append(~cs,Theta([CC | 0,0,0,0], tau : char := [[1,0,0,0],[1,0,0,0]], dz := [dz]));
 end for;
+
+basis := HolomorphicDifferentials(S);
+basis, M := Explode(basis);
+KCplane<uu,vv> := FunctionField(Cplane);
+sections := [Evaluate(el, [uu,vv]) : el in basis];
+diffs := Matrix(1,#sections,sections)*ChangeRing(M,Parent(sections[1]));
+phi := map< Cplane -> ProjectiveSpace(QQ,3) | Eltseq(diffs)>;
+Ccan := Image(phi);
+// Ccan; // LOL look at this thing
+
+Qs := [Evaluate(el, Eltseq(diffs)) : el in MonomialsOfDegree(R,2)];
+
+
+
+// next loop over all odd theta chars
